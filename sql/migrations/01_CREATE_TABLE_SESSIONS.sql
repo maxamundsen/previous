cb4e5b8bd76a304852b5da7f68e6a50b05cbd78d
Max Amundsen <max@cdrateline.com>
2024-01-24 00:13:55 -0500

    Add SQL migration scripts & info

---
+++ sql/migrations/01_CREATE_TABLE_SESSIONS.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `userid` varchar(255) NOT NULL,
+  `useragent` varchar(255) DEFAULT NULL,
+  `ipaddr` varchar(45) DEFAULT NULL,
+  `logintime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ sql/migrations/0_CREATE_TABLE_USERS.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
