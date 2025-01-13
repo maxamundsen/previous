2db7e740d479946fe0d2f5020db8b1621e29c419
Max Amundsen <max@cdrateline.com>
2025-01-13 09:48:36 -0500

    remove sqlx dependency

--- database/database.go
+++ database/database.go
@@ -4,22 +4,19 @@ import (
 	"log"
 	"previous/config"
 
-	"github.com/jmoiron/sqlx"
+	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // The database package provides an interface between go code and a relational database.
 
-var db *sqlx.DB
+var db *sql.DB
 
 func Init() {
 	var err error
 
-	db, err = sqlx.Connect("sqlite3", config.GetConfig().DbConnectionString)
-
-	// allow sqlx scan without all columns present
-	db = db.Unsafe()
+	db, err = sql.Open("sqlite3", config.GetConfig().DbConnectionString)
 
 	if err != nil {
 		panic(err.Error())
