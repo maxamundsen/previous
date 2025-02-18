1860d7e806b2b0eb3f1e2ab14474e55741cd229c
Max Amundsen <max@cdrateline.com>
2025-02-18 16:50:00 -0500

    minify css again

--- components/styles.go
+++ components/styles.go
@@ -1,12 +1,14 @@
 package components
 
 import (
+	"strings"
+
 	. "maragu.dev/gomponents"
 	. "maragu.dev/gomponents/html"
 )
 
 func InlineStyle(input string) Node {
-	// input = strings.ReplaceAll(input, "\n", "")
-	// input = strings.ReplaceAll(input, "\t", "")
+	input = strings.ReplaceAll(input, "\n", "")
+	input = strings.ReplaceAll(input, "\t", "")
 	return StyleEl(Raw(input))
 }
