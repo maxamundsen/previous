0d04bb8d7767bea6569bd853885b0de494e39d40
Max Amundsen <max@cdrateline.com>
2025-02-21 08:59:11 -0500

    forgot to add the files :/

---
+++ components/scripts.go
@@ -0,0 +1,55 @@
+package components
+
+import (
+	"fmt"
+	. "previous/basic"
+	"previous/config"
+	"strings"
+
+	. "maragu.dev/gomponents"
+	. "maragu.dev/gomponents/html"
+)
+
+// This component generates a <script> tag that automatically includes a blank scope { }.
+// Avoid using the javascript `var` keyword inside the scope as it will be placed in the global
+// scope.
+//
+// WARNING: DO NOT PASS USER INPUT TO THIS COMPONENT!!
+// THIS COULD BE A ONE WAY TICKET TO XSS IF USED IMPROPERLY!
+func InlineScript(script string) Node {
+	// minify in production only
+	if !config.DEBUG {
+		script = strings.ReplaceAll(script, "\n", " ")
+		script = strings.ReplaceAll(script, "\t", "")
+	}
+
+	return Script(Raw("{" + script + "}" ))
+}
+
+// Wrapper function around `InlineScript` that allows for printf style format strings + data
+//
+// WARNING: DO NOT PASS USER INPUT TO THIS COMPONENT!!
+// THIS COULD BE A ONE WAY TICKET TO XSS IF USED IMPROPERLY!
+func InlineScriptf(scriptFormat string, items ...interface{}) Node {
+	return InlineScript(fmt.Sprintf(scriptFormat, items...))
+}
+
+// Convert Go arrays to a string containing a javascript array.
+// Useful for injecting data into a dynamically generated script.
+func MakeJsArray[T any](list []T) string {
+	var out_string string
+
+	out_string += "["
+
+	for i, v := range list {
+		if i == len(list)-1 {
+			out_string += fmt.Sprintf("'%s'", ToString(v))
+		} else {
+			out_string += fmt.Sprintf("'%s',", ToString(v))
+		}
+	}
+
+	out_string += "]"
+
+	return out_string
+}
---
+++ handlers/app/examples/charts.go
@@ -0,0 +1,59 @@
+package examples
+
+import (
+	. "previous/components"
+	. "previous/handlers/app"
+
+	. "maragu.dev/gomponents"
+	. "maragu.dev/gomponents/html"
+
+	"previous/middleware"
+
+	"net/http"
+)
+
+func ChartHandler(w http.ResponseWriter, r *http.Request) {
+	identity := middleware.GetIdentity(r)
+
+	AppLayout("Chart.js Example", *identity,
+		BarChart(),
+	).Render(w)
+}
+
+func BarChart() Node {
+	label_string := MakeJsArray(
+		[]string{"test", "hello", "item3", "spiral"},
+	)
+
+	data_string := MakeJsArray(
+		[]int{3, 5, 8, 13},
+	)
+
+	return Div(
+		Canvas(),
+		InlineStyle(`
+			me {
+				background-color: var(--color-white);
+				padding: $(5);
+				border: 1px solid var(--color-neutral-200);
+				box-shadow: var(--shadow-md);
+			}
+		`),
+		InlineScriptf(`
+			let ctx = me().querySelectorRelative("canvas");
+
+			new Chart(ctx, {
+				type: 'bar',
+				data: {
+					labels: %s,
+					datasets: [{
+						data: %s,
+						borderWidth: 1
+					}]
+				},
+			});`,
+			label_string,
+			data_string,
+		),
+	)
+}
